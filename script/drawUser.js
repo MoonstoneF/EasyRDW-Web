f47c2ad4946abb7996f8c7d4891ec70de2707c71
fyy <fuyy21@mails.tsinghua.edu.cn>
2024-10-29 15:36:23 +0800

    Implement walk simulation

--- script/drawUser.js
+++ script/drawUser.js
@@ -4,19 +4,28 @@ let MAX_PATH_LEN = 100;
 // Environments
 const canvas_phys = document.getElementById('physCanvas');
 const canvas_virt = document.getElementById('virtCanvas');
-let border_phys = [{ x: 0, y: 0 }, { x: 0, y: 500 }, { x: 500, y: 0 }, { x: 500, y: 500 }];
-let border_virt = [{ x: 0, y: 0 }, { x: 0, y: 600 }, { x: 600, y: 0 }, { x: 600, y: 600 }];
+let border_phys = [{ x: 0, y: 0 }, { x: 0, y: 100 }, { x: 100, y: 0 }, { x: 100, y: 100 }];
+let border_virt = [{ x: 0, y: 0 }, { x: 0, y: 200 }, { x: 200, y: 0 }, { x: 200, y: 200 }];
 let obstacles_phys = [
-    [{ x: 100, y: 100 }, { x: 100, y: 200 }, { x: 200, y: 200 }, { x: 200, y: 100 }],
-    [{ x: 300, y: 300 }, { x: 300, y: 400 }, { x: 400, y: 400 }, { x: 400, y: 300 }]
+    [{ x: 20, y: 20 }, { x: 20, y: 40 }, { x: 40, y: 40 }, { x: 40, y: 20 }],
+    [{ x: 60, y: 60 }, { x: 60, y: 80 }, { x: 80, y: 80 }, { x: 80, y: 60 }]
 ];
 let obstacles_virt = []
 
 // User position and path in both environemnts
 let user_phys = { x: 50, y: 50, angle: 0, velocity: 0 };  // Initial user state
 let user_virt = { x: 50, y: 50, angle: 0, velocity: 0 };
-let path_phys = [{ x: user_phys.x, y: user_phys.y }];   // TODO: Log more user info
-let path_virt = [{ x: user_virt.x, y: user_virt.y }];
+
+// User config
+let poi = [{ x: 50, y: 150 }, { x: 150, y: 150 }, { x: 150, y: 50 }, { x: 50, y: 50 }]
+let poi_index = 0;
+let is_rotating = false;
+let walk_speed = 1;    // pixel/frame
+let turn_speed = 0.1;   // radius/frame
+
+// User log info
+let log_phys = [{ x: user_phys.x, y: user_phys.y }];   // TODO: Log more user info
+let log_virt = [{ x: user_virt.x, y: user_virt.y }];
 
 function drawPolygon(ctx, points) {
     // Find min max to get center
@@ -62,7 +71,6 @@ function drawPolygon(ctx, points) {
 // Draw environment
 function drawEnvironment(ctx, border, obstacles) {
     // Draw border
-    ctx.fillStyle = "aquamarine"
     drawPolygon(ctx, border);
     ctx.stroke();
 
@@ -96,8 +104,20 @@ function drawPhys() {
     ctx = canvas_phys.getContext('2d');
     ctx.clearRect(0, 0, canvas_phys.width, canvas_phys.height);
 
-    drawEnvironment(ctx, border_phys, obstacles_phys);
-    drawUser(ctx, user_phys, path_phys);
+    drawEnvironment(
+        ctx,
+        border_phys.map(element => ({ x: element.x * 5, y: element.y * 5 })),
+        obstacles_phys.map(a => (
+            a.map(element => (
+                { x: element.x * 5, y: element.y * 5 })
+            )
+        ))
+    );
+    drawUser(
+        ctx,
+        { x: user_phys.x * 5, y: user_phys.y * 5 },
+        log_phys.map(element => ({ x: element.x * 5, y: element.y * 5 }))
+    );
 }
 
 function drawVirt() {
@@ -105,9 +125,12 @@ function drawVirt() {
     ctx.clearRect(0, 0, canvas_virt.width, canvas_virt.height);
 
     drawEnvironment(ctx, border_virt, obstacles_virt);
-    drawUser(ctx, user_phys, path_phys);
+    drawUser(ctx, user_phys, log_phys);
 }
 
+// Test when no socket
+drawPhys();
+drawVirt();
 
 // -------------------------- WebSocket -----------------------------
 
@@ -134,11 +157,10 @@ function sendStartMsg() {
             border: border_virt,
             obstacle_list: obstacles_virt,
         }
-
-    }
+    };
 
     // Send start message
-    ws.send(JSON.stringify(start_msg))
+    ws.send(JSON.stringify(start_msg));
 }
 
 function sendRunMsg() {
@@ -160,6 +182,59 @@ function sendRunMsg() {
         user_v: user_virt.velocity,
         delta_t: 0.02,
         need_reset: need_reset
+    };
+}
+
+function sendEndMsg() {
+    const end_msg = {
+        type: "end",
+    };
+}
+
+function walk() {
+    if (poi_index >= poi.length) {
+        return false;
+    }
+
+    // Rotate/walk towards next poi with configured speed
+    const target_poi = poi[poi_index];
+    const target_direction = {
+        x: target_poi.x - user_virt.x,
+        y: target_poi.y - user_virt.y
+    };
+
+    const distance = Math.sqrt(target_direction.x ** 2 + target_direction.y ** 2);
+    const target_angle = Math.atan2(target_direction.y, target_direction.x); // Angle to the target
+
+    // Check if the walker is rotating or moving
+    if (is_rotating) {
+        // Rotate towards the target angle
+        user_direction += turn_speed;
+        if (Math.abs(user_direction - target_angle) < 0.01) {
+            user_direction = target_angle; // Snap to the target angle
+            is_rotating = false; // Stop rotating
+            // console.log(`Finished rotating towards POI: ${JSON.stringify(targetPOI)}`);
+        }
+    } else {
+        // Move towards the target POI if not rotating
+        if (distance > walk_speed) {
+            const normalizedDirection = {
+                x: target_direction.x / distance,
+                y: target_direction.y / distance
+            };
+
+            user_virt.x += normalizedDirection.x * walk_speed;
+            user_virt.y += normalizedDirection.y * walk_speed;
+        } else {
+            // Move to the target POI
+            user_virt = { ...targetPOI };
+            console.log(`Arrived at POI: ${JSON.stringify(targetPOI)}`);
+            is_rotating = true; // Start rotating towards the next POI
+            poi_index++;
+            if (poi_index < poi.length) {
+                console.log(`Now rotating towards next POI: ${JSON.stringify(this.pois[this.currentPOIIndex])}`);
+            }
+        }
     }
 }
 
@@ -180,18 +255,23 @@ ws.onmessage = (event) => {
             break;
 
         case "running":
-            // Receive new user physical position
+            // Draw user virtual position for this frame
+            if (log_virt.push({ x: user_virt.x, y: user_virt.y }) > MAX_PATH_LEN)
+                log_virt.shift();  // Update virtual path
+            drawVirt();
+
+            // Receive user physical position for this frame
             user_phys = { x: msg.user_x, y: msg.user_y, angle: msg.user_direction }
-            if (path_phys.push({ x: user_phys.x, y: user_phys.y }) > MAX_PATH_LEN)
-                path_phys.shift();  // Update physical path
+            if (log_phys.push({ x: user_phys.x, y: user_phys.y }) > MAX_PATH_LEN)
+                log_phys.shift();  // Update physical path
             drawPhys();
 
-            // TODO: New user virtual position
-            // Rotate/walk towards next poi with configured speed
-
-            if (path_virt.push({ x: user_virt.x, y: user_virt.y }) > MAX_PATH_LEN)
-                path_virt.shift();  // Update virtual path
-            drawVirt();
+            // New user virtual position for next frame
+            if (!walk()) {
+                // Finish simulation
+                sendEndMsg();
+                break;
+            }
 
             // Send next frame
             sendRunMsg();
@@ -201,7 +281,6 @@ ws.onmessage = (event) => {
             // end simulation
             ws.close(1, "End of simulation");
             break;
-
     }
 
 };
@@ -209,7 +288,3 @@ ws.onmessage = (event) => {
 ws.onclose = () => {
     console.log('WebSocket connection closed');
 };
-
-// Test when no socket
-drawPhys();
-drawVirt();
